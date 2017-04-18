7d53971e47145cbaf1822d80560037d62289173f
angcyo <angcyo@126.com>
2017-04-18 12:05:18 +0800

    add so support

--- bmob/src/main/java/com/angcyo/bmob/RBmob.java
+++ bmob/src/main/java/com/angcyo/bmob/RBmob.java
@@ -3,6 +3,8 @@ package com.angcyo.bmob;
 import android.app.Application;
 import android.content.Context;
 
+import com.angcyo.uiview.utils.T_;
+
 import cn.bmob.v3.Bmob;
 
 /**
@@ -14,7 +16,12 @@ public class RBmob {
     public static Application context;
 
     public static void init(Context context) {
-        Bmob.initialize(context, "6588be35d60a2c1569fa1ee961930d1b");
+        try {
+            Bmob.initialize(context, "6588be35d60a2c1569fa1ee961930d1b");
+        } catch (Exception e) {
+            e.printStackTrace();
+            T_.show("不支持的设备.");
+        }
         RBmob.context = (Application) context;
     }
 }
